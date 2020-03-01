983cf6e6d7cb1002bfdc300d980407a947d5d07a
LeviSmithCodes <eyevel@gmail.com>
2020-02-29 22:51:34 -0700

    first iteration

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="app container-fluid">
+    <router-view />
   </div>
 </template>
 
@@ -15,6 +11,14 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.app {
+  background: url("https://cdn.pixabay.com/photo/2016/07/08/11/32/texture-1504100_1280.jpg")
+    repeat center;
+  background-size: cover;
+  min-height: 100vh;
+  /* opacity: 0.6; */
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container-fluid">
+    <h1>Welcome to the Denzil-Reader</h1>
+    <form>
+      <input type="textarea" v-model="textBody" rows="4" cols="50" />
+    </form>
+    <br />
+    <p>
+      <b>{{textBody}}</b>
+    </p>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      textBody: "input text"
+    };
+  },
+  components: {}
+};
 </script>
+
+<style scoped>
+h1 {
+  color: black;
+  opacity: 1 !important;
+}
+
+p {
+  color: black;
+  font-size: 1.5rem;
+}
+</style>
